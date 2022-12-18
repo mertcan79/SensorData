4f87b52cae6715045498c49844c75071ef17666a
Mertcan Coskun <38556356+mertcan79@users.noreply.github.com>
2022-12-18 23:40:41 +0200

    Add files via upload

---
+++ scripts.sql
@@ -0,0 +1,27 @@
+create table sensors (id int, type text, timestamp text, value float);
+
+
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'temp', '2022-12-12 12:34:56.123456', 50.0);
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'temp', '2022-12-12 12:36:56.123456', 20.0);
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'temp', '2022-12-12 12:37:56.123456', 70.0);
+
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'temp', '2022-12-12 12:38:56.123456', 50.0);
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'temp', '2022-12-12 12:39:56.123456', 20.0);
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'temp', '2022-12-12 12:40:56.123456', 10.0);
+
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'temp', '2022-12-12 12:41:56.123456', 30.0);
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'temp', '2022-12-12 12:42:56.123456', 22.0);
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'temp', '2022-12-12 12:43:56.123456', 55.0);
+
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'temp', '2022-12-12 12:44:56.123456', 62.0);
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'temp', '2022-12-12 12:45:56.123456', 91.0);
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'temp', '2022-12-12 12:46:56.123456', 22.0);
+
+
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'hum', '2022-12-16 12:34:56.123456', 54.0);
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'hum', '2022-12-16 13:36:56.123456', 33.0);
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'hum', '2022-12-16 13:38:56.123456', 40.0);
+
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'hum', '2022-12-17 18:38:56.123456', 70.0);
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'hum', '2022-12-17 18:39:56.123456', 50.0);
+INSERT INTO sensors (id, type, timestamp, value) VALUES (1,'hum', '2022-12-17 18:40:56.123456', 20.0);
